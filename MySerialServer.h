7fc340e09cf9b1e03a3b29afe67f0f1b7d05804f
mohamadzah <59395490+mohamadzah@users.noreply.github.com>
2020-01-13 07:17:42 +0200

    Add files via upload
    Initialization of the files and getting grasp of what i need to do.

---
+++ MySerialServer.h
@@ -0,0 +1,16 @@
+//
+// Created by zahalka on 13/01/2020.
+//
+
+#ifndef MILESTONE_2_MYSERIALSERVER_H
+#define MILESTONE_2_MYSERIALSERVER_H
+
+#include "Server.h"
+
+class MySerialServer : public server_side::Server {
+public:
+
+};
+
+
+#endif //MILESTONE_2_MYSERIALSERVER_H
---
+++ Solver.h
@@ -0,0 +1,12 @@
+//
+// Created by zahalka on 13/01/2020.
+//
+
+#ifndef MILESTONE_2_SOLVER_H
+#define MILESTONE_2_SOLVER_H
+
+class Solver {
+
+};
+
+#endif //MILESTONE_2_SOLVER_H
