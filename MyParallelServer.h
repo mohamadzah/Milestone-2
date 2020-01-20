f9570328976769a93c3c5175047e0d0863f3e8d4
mohamadzah <59395490+mohamadzah@users.noreply.github.com>
2020-01-20 21:38:51 +0200

    Update on parallel server

--- MyParallelServer.h
+++ MyParallelServer.h
@@ -5,10 +5,16 @@
 #ifndef MILESTONE_2_MYPARALLELSERVER_H
 #define MILESTONE_2_MYPARALLELSERVER_H
 
+#include "Server.h"
+#include <vector>
 
-class MyParallelServer {
-
+class MyParallelServer : public server_side::Server {
+    vector<thread> threadVector;
+public:
+    MyParallelServer();
+    void open(int port, ClientHandler *c) override;
+    static void start(int port, ClientHandler *c);
+    void close() override;
 };
-
 
 #endif //MILESTONE_2_MYPARALLELSERVER_H
